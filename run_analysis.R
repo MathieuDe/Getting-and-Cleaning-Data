be5c318f325534f8bb7ae8ebc0273eabf98f96f8
Mathieu <mathieu.deleu@gmail.com>
2014-04-26 17:45:27 +0200

    Add features

--- run_analysis.R
+++ run_analysis.R
@@ -1,31 +1,40 @@
 #loading data
+## path where your files are
+path="E:/Coursera/Getting and Cleaning Data/UCI HAR Dataset"
+
 ##train
 ###read.table(file=, header=FALSE) in order to import data (there is no header on the .txt)
-x_train<-read.table("D:/Coursera/Getting and Cleaning Data/UCI HAR Dataset/train/X_train.txt",header=FALSE)
-y_train<-read.table("D:/Coursera/Getting and Cleaning Data/UCI HAR Dataset/train/Y_train.txt",header=FALSE)
-subject_train<-read.table("D:/Coursera/Getting and Cleaning Data/UCI HAR Dataset/train/subject_train.txt",header=FALSE)
+x_train<-read.table(file=paste(path,"/train/X_train.txt", sep = ""),header=FALSE)
+y_train<-read.table(file=paste(path,"/train/Y_train.txt", sep = ""),header=FALSE)
+subject_train<-read.table(file=paste(path,"/train/subject_train.txt", sep = ""),header=FALSE)
 ###combine y_train, subject_train and x_train by column
 train<-cbind(y_train, subject_train, x_train)
 
 ##test
-x_test<-read.table("D:/Coursera/Getting and Cleaning Data/UCI HAR Dataset/test/X_test.txt",header=FALSE)
-y_test<-read.table("D:/Coursera/Getting and Cleaning Data/UCI HAR Dataset/test/Y_test.txt",header=FALSE)
-subject_test<-read.table("D:/Coursera/Getting and Cleaning Data/UCI HAR Dataset/test/subject_test.txt",header=FALSE)
+x_test<-read.table(file=paste(path,"/test/X_test.txt", sep = ""),header=FALSE)
+y_test<-read.table(file=paste(path,"/test/Y_test.txt", sep = ""),header=FALSE)
+subject_test<-read.table(file=paste(path,"/test/subject_test.txt", sep = ""),header=FALSE)
 test<-cbind(y_test,subject_test, x_test)
 
 #combine test and train by row
 data<-rbind(train,test)
 
 #load columns names
-features<-read.table("D:/Coursera/Getting and Cleaning Data/UCI HAR Dataset/features.txt",header=FALSE,stringsAsFactors=FALSE)
-
+features<-read.table(file=paste(path,"/features.txt", sep = ""),header=FALSE,stringsAsFactors=FALSE)
+#load activity label names
+activity_label<-read.table(file=paste(path,"/activity_labels.txt", sep = ""),header=FALSE)
+colnames(activity_label)[1]<-"CodeActivity"
+colnames(activity_label)[2]<-"Activity_label"
+#merge activity labels with initial data (could have been done faster without a merge)
+data<-merge(activity_label, data, by.y="Activity", by.x="CodeActivity",sort=FALSE)
 #name colums
 ## naming measurement variables
 for (i in 1:561)
-	colnames(data)[i+2]<-(features[i,2])
-## naming manually activity and subject variables
+	colnames(data)[i+3]<-(features[i,2])
+## naming manually activities and subject variables
 colnames(data)[1]<-"Activity"
-colnames(data)[2]<-"Subject"
+colnames(data)[2]<-"Activity Label"
+colnames(data)[3]<-"Subject"
 	
 
 
@@ -34,14 +43,13 @@ z1<-grepl("mean()", colnames(data),fixed=TRUE)
 z2<-grepl("std()", colnames(data),fixed=TRUE)
 z<-z1|z2
 #Set the ID variables to TRUE
-z[1:2]<-TRUE
+z[2:3]<-TRUE
 
-#don't work	I wanted to exclUde meanFreq()and run grepl in one line
-#target<-c("mean()-","std()")
-#z0<-grepl(paste(target, collapse='|'), colnames(data))
+
 
 #create a tidy data set with only the measurements on the mean and standard deviation
 data2<-data[,colnames(data)[z]]
 
+
 #create a tidy data set with the average of each variable for each activity and each subject. 
-data_agg<-aggregate(data2, list("Activity/Subject" = paste(data2[,1],data2[,2])), mean)
+data_agg<-aggregate(data2[,(3:length(data2[1,]))], list("Activity/Subject" = paste(data2[,1],data2[,2])), mean)
