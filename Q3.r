22ac76e1b01ae105e69da5a060432d5ff684eff4
Mathieu <mathieu.deleu@gmail.com>
2014-04-26 15:27:30 +0200

    initial project version

---
+++ Q3.r
@@ -0,0 +1,21 @@
+D:/Coursera/Getting and Cleaning Data/getdata_data_EDSTATS_Country.csv
+D:/Coursera/Getting and Cleaning Data/getdata_data_GDP2.csv
+
+households on greater than 10 acres who sold more than $10,000 worth of agriculture products.
+
+
+ss06hid[ss06hid$ACR==3 & ss06hid$AGS==6,]
+125, 238,262
+
+source<-"D:/Coursera/Getting and Cleaning Data/getdata_jeff.jpg"
+j<-readJPEG(source, native = TRUE)
+quantile(j,seq(0,1,by=.1))
+-15259150 -10575416 
+
+ tapply(test[,2],test[,6],mean)
+
+ test[with(test, order(-test[,2])), 1:5]
+ 
+  
+ test[test$quant=="(1,38.6]" & test[,6]=='Lower middle income',var]
+ 
